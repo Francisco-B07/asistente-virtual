19b33180864b85c10241b1ad540b7b2b1b252035
Francisco Burgoa <franciscojburgoa@gmail.com>
2024-06-04 11:15:32 -0300

    limitar la memoria que TensorFlow puede utilizar de GPU

--- main.py
+++ main.py
@@ -45,6 +45,18 @@ templates = Jinja2Templates(directory="./public/templates")
 # Leer el archivo CSV
 df = pd.read_csv('recomendacion_supermercado_id.csv')
 
+# GPU, limita la memoria que TensorFlow puede utilizar
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        print(e)
+
+
 # ------------------------------FUNCIONES------------------------------
 
 # Carga la matriz de recomendaciones 
@@ -168,10 +180,7 @@ def train():
         class_mode='categorical',
         subset='training'
     )
-
  
-
-
     return train_generator
 
 
